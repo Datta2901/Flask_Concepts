8dc205748d73603daefab8610cc79189a2897609
manikanta2901 <manikanta2901@gmail.com>
2021-08-24 13:53:25 +0530

    main py file added

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask,redirect,url_for,render_template,request
+
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/success/<int:score>')
+def success(score):
+    res=""
+    if score>=50:
+        res="PASS"
+    else:
+        res='FAIL'
+    exp={'score':score,'res':res}
+    return render_template('result.html',result=exp)
+
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "The Person has failed and the marks is "+ str(score)
+
+### Result checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    result=""
+    if marks<50:
+        result='fail'
+    else:
+        result='success'
+    return redirect(url_for(result,score=marks))
+
+### Result checker submit html page
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        DSA=float(request.form['DSA'])
+        OS=float(request.form['OS'])
+        cv=float(request.form['cv'])
+        DAA=float(request.form['DAA'])
+        total_score=(DSA+OS+cv+DAA)/4
+    res=""
+    return redirect(url_for('success',score=total_score))
+
+
+if __name__=='__main__':
+    app.run(debug=True)
